0a9c0c1d3af02c97edc7386be368f3ae9d371320
jhonellamaceren <macerenjhonella@gmail.com>
2022-03-15 12:48:44 +0000

    Some form behaavior fixes to differentiate Add Record Form and Update Record Form

--- app/public/script.js
+++ app/public/script.js
@@ -3,14 +3,15 @@ const getRecords = document.getElementById("link-get-records");
 const formContainer = document.getElementById("form-container");
 const form = document.getElementById("post-form");
 const tableBody = document.getElementById("tbl-records-body");
+const formTitle = document.getElementById("form-title")
 
-/*Form Variables*/
-const firstName = document.getElementById("first-name");
-const lastName = document.getElementById("last-name");
-const email = document.getElementById("email");
-const age = document.getElementById("age");
-const salary = document.getElementById("salary");
-const address = document.getElementById("address");
+/*Form Input Variables*/
+const firstNameInput = document.getElementById("first-name");
+const lastNameInput = document.getElementById("last-name");
+const emailInput = document.getElementById("email");
+const ageInput = document.getElementById("age");
+const salaryInput = document.getElementById("salary");
+const addressInput = document.getElementById("address");
 
 //display records on page load
 displayRecords();
@@ -31,12 +32,12 @@ form.addEventListener('submit',function(e){
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      firstName: firstName.value,
-      lastName: lastName.value,
-      email: email.value,
-      age: age.value,
-      salary: salary.value,
-      address: address.value
+      firstName: firstNameInput.value,
+      lastName: lastNameInput.value,
+      email: emailInput.value,
+      age: ageInput.value,
+      salary: salaryInput.value,
+      address: addressInput.value
     })
   }).then(res => res.json())
   .then(displayRecords())
@@ -63,6 +64,28 @@ tableBody.addEventListener('click',(e)=>{
       .then(res => res.json())
       .then(displayRecords());
   }
+
+  if(updateBtnIsPressed){
+    //first parentElement directs to <td>, second one directs to <tr>
+    let row = e.target.parentElement.parentElement;
+    //get data from row
+    let rowFirstName = row.querySelector('#rowFirstName').textContent;
+    let rowLastName = row.querySelector('#rowLastName').textContent;
+    let rowEmail = row.querySelector('#rowEmail').textContent;
+    let rowAge = row.querySelector('#rowAge').textContent;
+    let rowSalary = row.querySelector('#rowSalary').textContent;
+    let rowAddress = row.querySelector('#rowAddress').textContent;
+
+    //show form and display data on it
+    showForm("Update Record");
+    firstNameInput.value = rowFirstName;
+    lastNameInput.value = rowLastName;
+    emailInput.value = rowEmail;
+    ageInput.value = rowAge;
+    salaryInput.value = rowSalary;
+    addressInput.value = rowAddress;
+
+  }
   
 });
 
@@ -74,13 +97,13 @@ getData().then(data => {
   data.map((records)=>{
       
       tableBodyContent += `<tr>
-          <td>${records.contactId}</td>
-          <td>${records.firstName}</td>
-          <td>${records.lastName}</td>
-          <td>${records.email}</td>
-          <td>${records.age}</td>
-          <td>${records.salary}</td>
-          <td>${records.address}</td>
+          <td id="rowContactId">${records.contactId}</td>
+          <td id="rowFirstName">${records.firstName}</td>
+          <td id="rowLastName">${records.lastName}</td>
+          <td id="rowEmail">${records.email}</td>
+          <td id="rowAge">${records.age}</td>
+          <td id="rowSalary">${records.salary}</td>
+          <td id="rowAddress">${records.address}</td>
           <td><button class="btn btn-primary" id="update-record"  data-id=${records.contactId}>Update</button></td>
           <td><button class="btn btn-danger"id="delete-record"  data-id=${records.contactId}>Delete</button></td>
           </tr>`;
@@ -89,12 +112,16 @@ getData().then(data => {
   tableBody.innerHTML = tableBodyContent;
 });
 }
-function showForm() {
+function showForm(title) {
     if (formContainer.style.display === "none") {
-      formContainer.style.display = "block";
-    } else {
-      formContainer.style.display = "none";
+      formContainer.style.display = "block";      
     }
+    if(title == "Add A New Record"){
+      form.reset();
+    }
+
+    //Change title depending on whether form is update or new record
+    formTitle.innerHTML = title;
   }
 
 
